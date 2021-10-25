62d86116440672d445b91977e5cd538b1f33c0e4
fateme <fateme3611@yahoo.com>
2021-10-25 19:22:41 -0400

    add users table seed

--- db/seeds/0.sql
+++ db/seeds/0.sql
@@ -1,1 +1,6 @@
-SELECT 1;
+INSERT INTO USERS 
+    (email, password, firstname, lastname)
+VALUES
+    ('email1', 'pass1', 'test1-name', 'test1-last'),
+    ('email2', 'pass2', 'test2-name', 'test1-last'),
+    ('email3', 'pass3', 'test3-name', 'test1-last');
