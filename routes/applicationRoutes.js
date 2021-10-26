21f1a3087b494d4bd7cda8cd4e7a240bb52246a3
Juan <juantvaldez85.@gmail.com>
2021-10-26 22:56:53 +0000

    add private quiz

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -1,16 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
-
 module.exports = () => {
-  
   router.get("/", (req, res) => {
     res.render("index");
   });
-      
+
   router.get("/questions", (req, res) => {
     res.render("quizQuestions");
+  });
+
+  router.get("/privatequiz", (req, res) => {
+    res.render("privateQuizzes");
   });
 
   return router;
