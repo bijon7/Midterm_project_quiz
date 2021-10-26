de0b59fc058046f9ddd159353e98b5ef94718fb5
fateme <fateme3611@yahoo.com>
2021-10-25 20:06:48 -0400

    add delete constraint

--- db/schema/5-quizResponses.sql
+++ db/schema/5-quizResponses.sql
@@ -1,7 +1,7 @@
 CREATE TABLE QuizeResponses(
     id SERIAL PRIMARY KEY NOT NULL,
-    user_id INT REFERENCES Users(id),
-    quiz_id INT REFERENCES Quizzes(id),
-    quiz_question_id INT REFERENCES QuizQuestions(id),
+    user_id INT REFERENCES Users(id) ON DELETE CASCADE,
+    quiz_id INT REFERENCES Quizzes(id) ON DELETE CASCADE,
+    quiz_question_id INT REFERENCES QuizQuestions(id) ON DELETE CASCADE,
     user_answer_id INT
 );
